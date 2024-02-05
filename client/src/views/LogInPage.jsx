0942fd159098762c719f49eaadaa35ef58c6b57a
davidvillardGit <davidvillard6@gmail.com>
2024-02-05 13:49:35 +0100

    add LogInPage

--- client/src/views/LogInPage.jsx
+++ client/src/views/LogInPage.jsx
@@ -2,10 +2,10 @@ export function LogInPage() {
     return (
       <div id="principal">
         <div className="container-login">
-            <div className="container-register"></div>
-            <div className="userTextBox"></div>
-            <div className="passwordTextBoX"></div>
-            <div className="submitButton"></div>
+            <div className="container-register">Registrarse</div>
+            <div className="userTextBox">Usuario</div>
+            <div className="passwordTextBoX">Contraseña</div>
+            <div className="submitButton">Enviar</div>
         </div>
       </div>
     );
