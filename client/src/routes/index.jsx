7de25ed12e73d42da2376af4b5e3df8106a12d37
Andrea Tena <tenaferandrea@gmail.com>
2024-02-05 11:07:55 +0100

    add insertar profesores y plantillas pages

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,11 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Ejemplo } from "../views/ejemplo.jsx";
+import { InsertarPlantillaPage } from "../views/InsertarPlantillaPage.jsx";
+import { InsertarProfesorPage } from "../views/InsertarProfesorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Ejemplo />,
   },
+  {
+    path: "/insertar",
+    element: <InsertarPlantilla888Page />,
+    children: [
+      {
+        path: "plantilla",
+        element: <InsertarPlantillaPage />,
+      },
+      {
+        path: "profesores",
+        element: <InsertarProfesorPage />,
+      },
+    ],
+  },
+
 ]);
 
 export default router;
