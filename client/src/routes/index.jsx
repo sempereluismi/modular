abf05d9959685d613dc6d227b7554f274de41ee9
Manuel <manuelchavesherrera69@gmail.com>
2024-02-05 11:08:41 +0100

    add: page quenda

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Ejemplo } from "../views/ejemplo.jsx";
+import { DirunoNocturnoPage } from "../views/DiurnoNocturnoPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Ejemplo />,
+  },
+  {
+    path: "/quenda",
+    element: <DirunoNocturnoPage />,
   },
 ]);
 
