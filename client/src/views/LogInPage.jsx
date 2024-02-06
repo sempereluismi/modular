ad94e9a12b9f5745d5dd124c397116eff0d7b159
Andrea Tena <tenaferandrea@gmail.com>
2024-02-06 11:17:40 +0100

    add Login sencillo

--- client/src/views/LogInPage.jsx
+++ client/src/views/LogInPage.jsx
@@ -1,12 +1,20 @@
 export function LogInPage() {
-    return (
-      <div id="principal">
+    return ( 
         <div className="container-login">
-            <div className="container-register">Registrarse</div>
-            <div className="userTextBox">Usuario</div>
-            <div className="passwordTextBoX">Contraseña</div>
-            <div className="submitButton">Enviar</div>
+          <h2>Iniciar sesión</h2>
+          <form action="procesar_login.php" method="post">
+            <div className="userTextBox">
+              <label htmlFor="usuario">Usuario:</label>
+              <input type="text" id="usuario" name="usuario" required></input>
+            </div>
+            <div className="passwordTextBoX">
+              <label htmlFor="contrasena">Contraseña:</label>
+              <input type="password" id="contrasena" name="contrasena" required></input>
+            </div>
+            <div className="submitButton">
+              <input type="submit" value="Enviar"></input>
+            </div> 
+          </form>
         </div>
-      </div>
     );
   }
