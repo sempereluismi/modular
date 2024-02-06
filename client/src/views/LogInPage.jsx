96302383721c312b5b629857102e25af325d1606
Andrea Tena <tenaferandrea@gmail.com>
2024-02-06 12:43:42 +0100

    add LogIn cutre

--- client/src/views/LogInPage.jsx
+++ client/src/views/LogInPage.jsx
@@ -1,13 +1,21 @@
 export function LogInPage() {
     return (
-      <div id="principal">
         <div className="container-login">
-            <div className="container-register">Registrarse</div>
-            <div className="userTextBox">Usuario</div>
-            <div className="passwordTextBoX">Contraseña</div>
-            <div className="submitButton">Enviar</div>
+          <form action="procesar_login.php" method="post">
+            <h2>Iniciar sesión</h2>
+            <div className="userTextBox">
+              <label for="usuario">Usuario:</label>
+              <input type="text" id="usuario" name="usuario" required/>
+            </div>
+            <div className="passwordTextBoX">
+              <label for="contrasena">Contraseña:</label>
+              <input type="password" id="contrasena" name="contrasena" required/>
+            </div>
+            <div className="submitButton">
+              <input type="submit" value="Enviar"/> 
+            </div>
+          </form>
         </div>
-      </div>
     );
   }
   
