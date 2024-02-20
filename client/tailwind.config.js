126c50dd268adf2f25c21b107fa9d056f95bce63
Manuel Alejandro Chaves Herrera <a22manuelach@ateis.local>
2024-02-20 14:07:36 +0100

    add SideBar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-purple': '#081A51',
+        'light-white': 'rgba(255,255,255,0.17)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
