2e303bc24e5a94cef2fa3a15fe9cec509f34b49c
David Villar Durán <a22davidvd@ATEIS.LOCAL>
2024-02-20 14:26:49 +0100

    add comprobación CSV

--- server/app/src/Application/Actions/CSV/ComprobacionCSV.php
+++ server/app/src/Application/Actions/CSV/ComprobacionCSV.php
@@ -2,30 +2,32 @@
 
 namespace App\Application\Actions\CSV;
 
+use App\Application\Actions\Controller;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class ComprobacionCSV
+class ComprobacionCSV extends Controller
 {
-
     public function uploadFiles(Request $request, Response $response)
     {
         // Obtenemos los archivos subidos
         $uploadedFiles = $request->getUploadedFiles();
+
         // Verificamos que se haya enviado un archivo y es un CSV
         if (isset($uploadedFiles['csvFile']) && $uploadedFiles['csvFile']->getError() === UPLOAD_ERR_OK) {
             if ($this->esArchivoCSV($uploadedFiles['csvFile'])) {
                 // Procesa el archivo csv
-                $response->getBody()->write('Archivo CSV válido');
+                return $this->returnResponse($response, ["success" => "Formato de archivo valido"], 200);
             } else {
-                $response->getBody()->write('El archivo no es un CSV válido');
+                return $this->returnResponse($response, ["error" => "Formato de archivo incorrecto"], 400);
             }
         } else {
-            $response->getBody()->write('No se ha enviado ningún archivo o hay un error en la carga');
+            return $this->returnResponse($response, ["error" => "No se ha enviado ningún archivo o hay un error en la carga"], 400);
         }
 
         return $response;
     }
+
     private function esArchivoCSV($uploadedFile): bool
     {
         // Verifica la extensión del archivo
