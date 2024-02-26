b246fcece070bdb40e775ddb6c5843a6ae0759cb
davidvillardGit <davidvillard6@gmail.com>
2024-02-26 14:44:48 +0100

    sync

---
+++ client/src/components/CardNoteBookElements.jsx
@@ -0,0 +1,27 @@
+function CardNoteBookElements () {
+  return (
+    <div className='flex flex-col p-2 gap-1'>
+      <div className='w-full relative'>
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5 mt-10' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-blue-500/70 w-full h-[2px] rounded-full mb-5' />
+        <div className='bg-red-500 absolute top-0 left-[50px] w-[2px] h-[376px] rounded-full' />
+      </div>
+    </div>
+  )
+}
+
+export default CardNoteBookElements
