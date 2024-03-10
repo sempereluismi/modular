e8ba5ff06375bccbda1433f6af5ea9c72148811f
sempereluismi <sempereluismiguel@gmail.com>
2024-03-10 17:02:10 +0100

    add insertar profesror

--- client/src/components/DragAndDrop.jsx
+++ client/src/components/DragAndDrop.jsx
@@ -1,17 +1,39 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react'
+import React from 'react'
 import { FileUploader } from 'react-drag-drop-files'
 import { DragAndDropDesing } from '../components/DragAndDropDesing.jsx'
-
+import { useAuth } from '../hooks/useAuth.jsx'
 const fileTypes = ['CSV']
 
 function DragDrop ({ urlImage }) {
-  const [file, setFile] = useState(null)
-  const handleChange = (file) => {
-    setFile(file)
+  const { user } = useAuth()
+
+  const handleChange = async (file) => {
+    const formData = new FormData()
+    formData.append('csvFile', file)
+
+    try {
+      const response = await fetch(`http://localhost:8000/api/upload/profesor/${user.id_departamento}`, {
+        method: 'POST',
+        body: formData
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        console.log(data)
+        // Aquí puedes manejar la respuesta de la API si lo deseas
+      } else {
+        const data = await response.json()
+        console.log(data)
+        console.error('Failed to upload file', data)
+      }
+    } catch (error) {
+      console.error('Error uploading file:', error)
+    }
   }
+
   return (
-    <FileUploader handleChange={handleChange} name='file' types={fileTypes}>
+    <FileUploader handleChange={handleChange} name='csvFile' types={fileTypes}>
       <DragAndDropDesing urlImage={urlImage} />
     </FileUploader>
   )
