3243316c62330a73c999ba3e0959cfa807a87ea9
lsempere <lsempere@sealabsolutions.com>
2024-06-05 13:49:38 +0200

    Add: peticiones para recoger los modelos

--- client/src/views/InsertarPlantillaPage.jsx
+++ client/src/views/InsertarPlantillaPage.jsx
@@ -1,6 +1,18 @@
+import { useContext, useEffect } from 'react'
 import { Layout } from '../layouts/Layout'
+import { getAdminModels, getProfesorModels } from '../service/csv'
+import { AuthContext } from '../context/AuthContext'
 
 export function InsertarPlantillaPage () {
+  const { user } = useContext(AuthContext)
+
+  useEffect(() => {
+    Promise.all([getAdminModels(user.id_departamento), getProfesorModels(user.id)])
+      .then(([adminModels, profesorModels]) => {
+        console.log(adminModels, profesorModels)
+      })
+  }, [])
+
   return (
     <Layout>
       <h1>Insertar Plantilla</h1>
