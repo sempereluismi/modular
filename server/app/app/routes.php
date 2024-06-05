ffa2c6e21e4b04337bfb0ae37075a9b2e11fb06b
mmanuch <mchaves@gradiant.org>
2024-06-05 13:32:39 +0200

    server/app/app/routes.php comentario

--- server/app/app/routes.php
+++ server/app/app/routes.php
@@ -14,6 +14,36 @@ use App\Application\Actions\CSV\ComprobacionCSV;
 use App\Application\Actions\Login\LoginController;
 
 
+/**
+ * Esta es la sección de enrutamiento
+ * 
+ * Ruta '/{routes:.*}':
+ * Maneja solicitudes OPTIONS para todas las rutas (CORS Pre-Flight).
+ * 
+ * Grupo de rutas base '/api':
+ * 
+ *   Grupo de rutas '/modulos':
+ *     - GET '': Lista todos los módulos (Metodo: listarModulo en ModuloController).
+ *     - GET '/{id}': Lista un módulo específico por ID (Metodo: listarModulo en ModuloController).
+ * 
+ *   Grupo de rutas '/profesor':
+ *     - GET '': Lista todos los profesores (Metodo: listarProfesor en ProfesorController).
+ *     - GET '/{id}': Lista un profesor específico por ID (Metodo: listarProfesor en ProfesorController).
+ * 
+ *   Grupo de rutas '/csv':
+ *     - POST '/upload/{id}': Sube archivos CSV por ID (Metodo: uploadFiles en ComprobacionCSV).
+ *     - POST '/save-model/{id}': Guarda modelos de archivos CSV por ID (Metodo: saveFiles en ComprobacionCSV).
+ *     - GET '/list/{id}': Lista archivos CSV por ID (Metodo: listFiles en ComprobacionCSV).
+ * 
+ *   Grupo de rutas '/auth':
+ *     - POST '/login': Iniciar sesión (Metodo: login en LoginController).
+ *     - GET '/rol/{id}': Obtener rol por ID (Metodo: rol en LoginController).
+ * 
+ *   Grupo de rutas '/regimen':
+ *     - GET '/{id}': Listar régimen por ID (Metodo: listarRegimen en RegimenController).
+ *     - POST '': Insertar un nuevo régimen (Metodo: inserirRegimen en RegimenController).
+ */
+
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
         // CORS Pre-Flight OPTIONS Request Handler
